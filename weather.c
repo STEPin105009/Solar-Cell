04ddce662885a89951fa121bfd2ac281217c6350
STEPin105009 <69211486+STEPin105009@users.noreply.github.com>
2020-09-18 17:18:41 +0530

    Add files via upload

---
+++ weather.c
@@ -0,0 +1,14 @@
+//.c file
+#include <stdio.h>
+
+void weather() {
+
+   float humid;
+   printf("enter weather condition");
+   scanf("%f",&weathcon);
+   if (weathcon<20)
+   {
+	printf("Cloudy or night");
+    }
+   return 0;
+}
